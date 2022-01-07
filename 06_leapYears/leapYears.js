e761df425b562cd981b9f4cf6dc5b1a83d588e8b
Doruk Emre <devdoruk@outlook.com>
2022-01-07 23:07:35 +0000

    Modified leapYears.js with solution for ex6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true
+        } else if (year % 100 === 0 && year % 400 !== 0) {
+            return false
+        }
+        return true
+    } 
+    return false
 
 };
 
+/*
+console.log(leapYears(1996)); //true
+console.log(leapYears(1997)); // false
+console.log(leapYears(34992)); // true
+console.log(leapYears(1900)); // false
+console.log(leapYears(1600)); // true
+console.log(leapYears(700)); // false
+*/
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+/* Leap years are years divisible by four (like 1984 and 2004). 
+However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+unless they are divisible by 400 
+(like 1600 and 2000, which were in fact leap years) */
