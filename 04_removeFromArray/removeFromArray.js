00075526557884619c73124ebce1330a8d5a10d4
Doruk Emre <devdoruk@outlook.com>
2022-01-07 19:46:40 +0000

    Modified removeFromArray.js with solution to ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedItem) {
+    for (let i = 0 ; i < removedItem.length ; i++) {
+        
+        if (array.includes(removedItem[i]) === true) {
+            array.splice(array.indexOf(removedItem[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
