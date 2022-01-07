1d878f3cd5d3183bf8b1a781ff8c7aae088af9aa
Doruk Emre <devdoruk@outlook.com>
2022-01-07 17:04:06 +0000

    Added repeatString.js and modified test file exo2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let outputString = "";
+    for  (let i = 0; i < num; i++) {
+        outputString += string;
+    }
+    if (num >=0) {
+    return outputString;
+    }
+    return "ERROR"
 };
 
 // Do not edit below this line
