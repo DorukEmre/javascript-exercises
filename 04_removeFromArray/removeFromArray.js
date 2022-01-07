0ae7fe637504d4b0d7494d499b4f1eb495ceadcc
Doruk Emre <devdoruk@outlook.com>
2022-01-07 23:51:11 +0000

    Modified removeFromArray.js with official solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -10,3 +10,18 @@ const removeFromArray = function(array, ...removedItem) {
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// official solution: check the array and if it's not in args push it to newArray.
+const removeFromArray = function (...args) {
+    const array = args[0];
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+        });
+      
+  return newArray;
+};
