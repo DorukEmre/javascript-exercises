ef1cd6735e277ad8b00041662a0b802c93dd2ae3
Doruk Emre <devdoruk@outlook.com>
2022-01-07 21:12:35 +0000

    Modified sumAll.js with solution for ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    let sum = 0;
+    
+    if (firstNumber < 0 || lastNumber < 0 || typeof firstNumber !== "number" || typeof lastNumber !== "number") {
+        return "ERROR"
+    } else if (firstNumber < lastNumber) {
+        for (let i = 0 ; i < (lastNumber - firstNumber +1) ; i++) {
+            sum = sum + firstNumber + i;
+        }
+    } else if (firstNumber > lastNumber) {
+        for (let i = 0 ; i < (firstNumber - lastNumber +1) ; i++) {
+            sum = sum + lastNumber + i;
+        }
+    } else {
+        return " other ERROR"
+    }
+
+    return sum;
 
 };
 
