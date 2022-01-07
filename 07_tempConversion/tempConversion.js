02eea5b17563ea698caa8f622304e5c864fcedd7
Doruk Emre <devdoruk@outlook.com>
2022-01-07 23:50:24 +0000

    Modified tempConversion.js with solution for ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9
+   return celsius = Math.round(celsius * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32 
+  return fahrenheit = Math.round(fahrenheit * 10) / 10
 };
+
+
+ftoc(-100) // fahrenheit to celsius, should return 0
+
+ctof(99) // celsius to fahrenheit, should return 32
+
 
 // Do not edit below this line
 module.exports = {
