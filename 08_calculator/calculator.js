4df831e10706aec98963ae8e3760c1906e8d4a6e
Doruk Emre <devdoruk@outlook.com>
2022-01-08 01:23:47 +0000

    Modified calculator.js with solution for ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayItems) {
+	let sumArray = 0;
+  arrayItems.forEach(function(arrayItem) {
+    sumArray += arrayItem;
+  })
+  return sumArray;
 };
 
-const multiply = function() {
+/* Official solution
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+*/
 
+const multiply = function(arrayItems) {
+	let multiplyArray = 1;
+  arrayItems.forEach(function(arrayItem) {
+    multiplyArray *= arrayItem;
+  })
+  return multiplyArray;
 };
 
-const power = function() {
-	
+/* Official solution
+const multiply = function(array) {
+    return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
+};
+*/
+
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(items) {
+  let fact = 1;
+
+  for (let i = 1; i <= items; i++) {
+    fact *= i
+  }
+  return fact;
 };
+
 
 // Do not edit below this line
 module.exports = {
