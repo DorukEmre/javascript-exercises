f30a93310b47695f9ad391a23a0538d076eafef5
Doruk Emre <devdoruk@outlook.com>
2022-01-08 19:04:51 +0000

    Modified palindromes.js with solution to ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function(inputStr) {
+    
+    let reverseStr = "";
+    let newInputStr = inputStr.replace(/[^\w]/g, "").toLowerCase();
+    
+    for (let i = 0; i < newInputStr.length; i++) {
+        reverseStr += newInputStr[newInputStr.length-i-1];
+    }
 
+    if (newInputStr === reverseStr) {
+        return true
+    }
+    return false
 };
+
+
+palindromes('Animal loots foliated detail of stool lamina.') // true
+// palindromes('Tac.os') // false
 
 // Do not edit below this line
 module.exports = palindromes;
