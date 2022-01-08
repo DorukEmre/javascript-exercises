9abc1c339f09ce81ce9240d7a93963a218704993
Doruk Emre <devdoruk@outlook.com>
2022-01-08 19:24:58 +0000

    Modified fibonacci.js with solution to ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let fibo = [1, 1];
+    numberInput = parseInt(input, 10)
 
+    if (numberInput < 0) return "OOPS"
+
+    for (let i = 1; i < numberInput; i++) {
+        fibo.push(fibo[i] + fibo[i-1])
+    }
+    return fibo[numberInput-1]
+    
 };
 
 // Do not edit below this line
