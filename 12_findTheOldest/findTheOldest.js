1029e1fbaf2ed01ceb79f2f0288a1911cb8c9dfb
Doruk Emre <devdoruk@outlook.com>
2022-01-24 21:52:35 +0000

    Modified findTheOldest.js with solution for ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b) => {
+        if (a.hasOwnProperty("yearOfDeath") === false ||
+        b.hasOwnProperty("yearOfDeath") === false) {
+            if (a.hasOwnProperty("yearOfDeath") === false) {
+                aAge = new Date().getFullYear() - a.yearOfBirth
+                bAge = b.yearOfDeath - b.yearOfBirth
+                return aAge > bAge ? -1 : 1;
+            } else if (b.hasOwnProperty("yearOfDeath") === false) {
+                aAge = a.yearOfDeath - a.yearOfBirth
+                bAge = new Date().getFullYear() - b.yearOfBirth
+                return aAge > bAge ? -1 : 1;
+            }
+        }
+        aAge = a.yearOfDeath - a.yearOfBirth
+        bAge = b.yearOfDeath - b.yearOfBirth
+        return aAge > bAge ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
