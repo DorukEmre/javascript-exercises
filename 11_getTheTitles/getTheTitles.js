2a9f03d904d791c785edc8c63445df108246018f
Doruk Emre <devdoruk@outlook.com>
2022-01-24 21:09:39 +0000

    Modified getTheTitles.js with solution for ex11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
 
+
+
+const getTheTitles = function(input) {
+    return input.map(function(book) {
+          return book.title
+    })
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
